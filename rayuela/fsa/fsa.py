eb1df79995c648d0ece9471612e37a7e206f9f43
Fengshi Zheng <xfysjames@icloud.com>
2022-03-08 23:16:36 +0100

    Assignment 1 question 2

--- rayuela/fsa/fsa.py
+++ rayuela/fsa/fsa.py
@@ -600,3 +600,35 @@ class FSA:
 		''')
 
 		return ''.join(ret2)
+
+
+if __name__ == '__main__':
+    fsa = FSA(Boolean)
+
+    # We can directly add edges between the states without adding the states first.
+    # The states will be created automatically.
+    fsa.add_arc(State(1), Sym('a'), State(2))
+    fsa.add_arc(State(1), Sym('b'), State(3))
+
+    fsa.add_arc(State(2), Sym('b'), State(2))
+    fsa.add_arc(State(2), Sym('c'), State(4))
+
+    fsa.add_arc(State(3), Sym('c'), State(4))
+    fsa.add_arc(State(3), Sym('b'), State(5))
+
+    fsa.add_arc(State(4), Sym('a'), State(6))
+    fsa.add_arc(State(5), Sym('a'), State(6))
+
+    # Add initial and final states
+    fsa.set_I(State(1))
+    fsa.set_F(State(6))
+
+    assert fsa.deterministic
+    assert fsa.pushed
+
+    fsa.add_arc(1, 'a', 3)
+    assert not fsa.deterministic
+    assert fsa.pushed
+
+    fsa.add_state(7)
+    assert not fsa.pushed
